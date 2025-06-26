50a37f1e60149e32d233107c91b16ff3cbf5563f
Ezequiel0117 <ejaramilloy@unemi.edu.ec>
2025-06-25 22:27:03 -0500

    Reduccion de texto

--- clasificador/admin.py
+++ clasificador/admin.py
@@ -4,9 +4,9 @@ from .models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ('username', 'email', 'first_name', 'last_name', 'phone', 'is_active', 'date_joined')  # Campos que se mostrarán
-    search_fields = ('username', 'email', 'phone')  # Campos por los que se puede buscar
-    list_filter = ('is_active', 'is_staff', 'is_superuser')  # Filtros disponibles
-    ordering = ('date_joined',)  # Orden por defecto
+    list_display = ('username', 'email', 'first_name', 'last_name', 'phone', 'is_active', 'date_joined')
+    search_fields = ('username', 'email', 'phone')
+    list_filter = ('is_active', 'is_staff', 'is_superuser')
+    ordering = ('date_joined',)
 
 admin.site.register(CustomUser, CustomUserAdmin)
